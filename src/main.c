17a1200a336e85c214eec45886a559a5246dbbd8
M2U7BF <yhvh4321@gmail.com>
2025-05-29 12:18:59 +0900

    引数管理を実装

--- src/main.c
+++ src/main.c
@@ -6,13 +6,17 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:39:07 by kkamei            #+#    #+#             */
-/*   Updated: 2025/05/29 11:40:40 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/05/29 12:12:23 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
+	(void)argv;
+	(void)envp;
+	if (argc != 1)
+		return (1);
 	return (0);
 }
