d2f883df04cfb7e6f65ffa9afd5607eef9cf1a05
M2U7BF <yhvh4321@gmail.com>
2025-06-02 17:52:33 +0900

    update

--- src/main.c
+++ src/main.c
@@ -6,11 +6,36 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:39:07 by kkamei            #+#    #+#             */
-/*   Updated: 2025/06/02 17:16:34 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/06/02 17:45:56 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+char *redirect_expansion(char *s)
+{
+  // 
+}
+
+char *pipe_expansion(char *s)
+{
+  // 
+}
+
+char *variable_expansion(char *s)
+{
+	// $を探す
+	// 変数と一致するか検索する
+	// 一致し次第、置換し、文字列を更新する
+}
+
+char *parse(t_vars	*vars)
+{
+  variable_expansion(vars->input_line);
+	// TODO プロセス数を判定後、下記を実施。
+	redirect_expansion(vars->input_line);
+	pipe_expansion(vars->input_line);
+}
 
 t_vars	*init_vars(void)
 {
@@ -42,6 +67,7 @@ int	main(int argc, char *argv[], char *envp[])
 		// 履歴への追加
 		add_history(vars->input_line);
 		// パース
+    parse(vars);
 		// コマンド実行
 		ft_free(vars->input_line);
 	}
