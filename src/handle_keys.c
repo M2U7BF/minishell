787dd3c0a978ddacce037026c8753aa2e70043b7
Atsuya Tashiro <atashiro@c1r4s8.42tokyo.jp>
2025-06-16 12:20:41 +0900

    Ctrl+\を無視,グローバル変数を追加

--- src/handle_keys.c
+++ src/handle_keys.c
@@ -1,32 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_signal.c                                    :+:      :+:    :+:   */
+/*   handle_keys.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 16:55:12 by kkamei            #+#    #+#             */
-/*   Updated: 2025/06/02 17:12:08 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/06/16 12:14:15 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	ctrl_backslash(int signum)
-{
-	// TODO 内容実装
-	printf("ctrl_backslash, signum:%d\n", signum);
-}
+volatile	sig_atomic_t g_recieve_signal = 0;;
+// void	ctrl_backslash(int signum)
+// {
+// 	// TODO 内容実装
+// 	printf("ctrl_backslash, signum:%d\n", signum);
+// }
 
-void	ctrl_c(int signum)
+// void	ctrl_c(int signum)
+// {
+// 	// TODO 内容実装
+// 	(void)signum;
+// 	exit(EOWNERDEAD);
+// }
+
+/*受け取ったシグナルの番号をグルーバル変数に入れる*/
+void	signal_hander(int signum)
 {
-	// TODO 内容実装
-	(void)signum;
-	exit(EOWNERDEAD);
+	g_recieve_signal = signum;
 }
 
 void	handle_signal(void)
 {
-	signal(SIGINT, ctrl_c);
-	signal(SIGQUIT, ctrl_backslash);
+	signal(SIGINT, signal_hander);
+	signal(SIGQUIT, SIG_IGN);
 }
