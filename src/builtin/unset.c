99a54685184d3f531ef73aa0a6e0d6d8e9899a58
Atsuya Tashiro <atashiro@c1r4s7.42tokyo.jp>
2025-06-25 16:49:29 +0900

    fin

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 15:44:57 by atashiro          #+#    #+#             */
-/*   Updated: 2025/06/25 13:47:40 by atashiro         ###   ########.fr       */
+/*   Updated: 2025/06/25 16:35:21 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,82 +32,113 @@ static int	is_valid_identifier(const char *s)
 	return (1);
 }
 
-static int	find_env_var_index(const char *name)
+int builtin_unset(char **argv)
 {
-	int		i;
-	size_t	len;
-
-	i = 0;
-	len = ft_strlen(name);
-	while (environ[i])
-	{
-		if (ft_strncmp(environ[i], name, len) == 0)
-		{
-			if (environ[i][len] == '=' || environ[i][len] == '\0')
-				return (i);
-		}
-		i++;
-	}
-	return (-1);
-}
-
-static int	remove_env_var_by_index(int index_to_remove)
-{
+	int		old_count;
+	char	**new_environ;
 	int		i;
 	int		j;
-	int		count;
-	char	**new_environ;
+	int		status;
 
-	count = 0;
-	while (environ[count])
-		count++;
-	new_environ = malloc(sizeof(char *) * count);
-	if (!new_environ)
-		return (-1);
-	i = 0;
-	j = 0;
-	while (i < count)
-	{
-		if (i != index_to_remove)
-		{
-			new_environ[j] = environ[i];
-			j++;
-		}
-		i++;
-	}
-	new_environ[j] = NULL;
-	free(environ[index_to_remove]);
-	free(environ);
-	environ = new_environ;
-	return (0);
-}
-
-int	builtin_unset(char **argv)
-{
-	int	i;
-	int	status;
-	int	found_index;
-
-	i = 1;
 	status = 0;
-	while (argv[i])
+	i = 1;
+	while(argv[i]) // 先に引数のバリデーションを行う
 	{
 		if (!is_valid_identifier(argv[i]))
 		{
-			ft_dprintf(STDERR_FILENO,
-				"minishell: unset: `%s': not a valid identifier\n", argv[i]);
+			ft_dprintf(STDERR_FILENO, "minishell: unset: `%s': not a valid identifier\n", argv[i]);
 			status = 1;
-		}
-		else
-		{
-			found_index = find_env_var_index(argv[i]);
-			if (found_index != -1)
-			{
-				if (remove_env_var_by_index(found_index) != 0)
-					status = 1;
-			}
 		}
 		i++;
 	}
+
+	old_count = 0;
+	while (environ[old_count])
+		old_count++;
+
+	// 新しい配列を確保 (最大で元のサイズと同じ)
+	new_environ = ft_calloc(old_count + 1, sizeof(char *));
+	if (!new_environ)
+		return (1);
+
+	i = 0;
+	j = 0;
+	while (environ[i])
+	{
+		int		k = 1;
+		int		should_unset = 0;
+		while (argv[k])
+		{
+			size_t len = ft_strlen(argv[k]);
+			if (is_valid_identifier(argv[k]) && ft_strncmp(environ[i], argv[k], len) == 0 && environ[i][len] == '=')
+			{
+				should_unset = 1;
+				break;
+			}
+			k++;
+		}
+		if (should_unset)
+			free(environ[i]); // 削除対象の文字列を解放
+		else
+			new_environ[j++] = environ[i]; // 保持する変数のポインタを新しい配列にコピー
+		i++;
+	}
+	new_environ[j] = NULL;
+	free(environ); // 古いポインタ配列を解放
+	environ = new_environ; // グローバルポインタを新しい配列に差し替える
 	return (status);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include "../../include/minishell.h"
+
+// char **environ;
+
+// void print_environ(const char *title)
+// {
+// 	int i = 0;
+// 	printf("---- %s ----\n", title);
+// 	while (environ[i])
+// 	{
+// 		printf("[%d] %s\n", i, environ[i]);
+// 		i++;
+// 	}
+// 	printf("--------------------\n");
+// }
+
+// // テスト用に環境変数を初期化する
+// void init_test_environ(void)
+// {
+// 	environ = malloc(sizeof(char *) * 5);
+// 	environ[0] = strdup("PATH=/usr/bin");
+// 	environ[1] = strdup("HOME=/home/user");
+// 	environ[2] = strdup("SHELL=/bin/bash");
+// 	environ[3] = strdup("USER=tester");
+// 	environ[4] = NULL;
+// }
+
+// int main(void)
+// {
+// 	char *argv1[] = {"unset", "HOME", "INVALID-NAME", NULL};
+// 	char *argv2[] = {"unset", "USER", "PATH", NULL};
+
+// 	init_test_environ();
+// 	print_environ("Before unset");
+
+// 	printf("Running builtin_unset(argv1):\n");
+// 	builtin_unset(argv1);
+// 	print_environ("After unset argv1");
+
+// 	printf("Running builtin_unset(argv2):\n");
+// 	builtin_unset(argv2);
+// 	print_environ("After unset argv2");
+
+// 	// メモリ解放
+// 	for (int i = 0; environ[i]; i++)
+// 		free(environ[i]);
+// 	free(environ);
+
+// 	return 0;
+// }
