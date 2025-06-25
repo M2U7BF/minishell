be7578f06403128584d6966569bc514c28728423
M2U7BF <yhvh4321@gmail.com>
2025-06-25 09:35:53 +0900

    errnoが使える部分は置き換え

--- src/file.c
+++ src/file.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 11:45:39 by kkamei            #+#    #+#             */
-/*   Updated: 2025/06/19 13:33:28 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/06/25 09:04:45 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,46 +29,24 @@ bool	is_readable_file(char *pathname)
 
 int	open_additionalfile(char *filename, int *fd)
 {
-	if (filename[0] == '\0')
-		return (ENOENT);
-	if (access(filename, F_OK) == 0)
-	{
-		if (access(filename, R_OK) == -1)
-			return (EACCES);
-		if (!is_readable_file(filename))
-			return (EISDIR);
-	}
 	*fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0664);
 	if (*fd == -1)
-		return (EXIT_FAILURE);
+		return (errno);
 	return (0);
 }
 
 int	open_outfile(char *filename, int *fd)
 {
-	if (filename[0] == '\0')
-		return (ENOENT);
-	if (access(filename, F_OK) == 0)
-	{
-		if (access(filename, R_OK) == -1)
-			return (EACCES);
-		if (!is_readable_file(filename))
-			return (EISDIR);
-	}
 	*fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (*fd == -1)
-		return (EXIT_FAILURE);
+		return (errno);
 	return (0);
 }
 
 int	open_infile(char *filename, int *fd)
 {
-	if (access(filename, F_OK) == -1)
-		return (ENOENT);
-	if (access(filename, R_OK) == -1)
-		return (EACCES);
 	*fd = open(filename, O_RDONLY);
 	if (*fd == -1)
-		return (EXIT_FAILURE);
+		return (errno);
 	return (0);
 }
