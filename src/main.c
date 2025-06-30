c87db15db5fa210ef1d43f825a8a417dbcd0cc64
Atsuya Tashiro <atashiro@c1r4s7.42tokyo.jp>
2025-06-30 13:49:10 +0900

    コマンドライン引数にenvを追加,環境変数のリストを初期化

--- src/main.c
+++ src/main.c
@@ -3,27 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:39:07 by kkamei            #+#    #+#             */
-/*   Updated: 2025/06/24 15:42:33 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/06/30 12:23:42 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int argc, char *argv[])
+#include "../include/minishell.h"
+
+// main関数に `char **envp` を追加
+int	main(int argc, char *argv[], char **envp)
 {
 	t_exec_vars	e_vars;
 
 	if (argc > 3)
 		return (EXIT_FAILURE);
 	init_e_vars(&e_vars);
+	init_env_list(&e_vars.env_list, envp); // <-- 環境変数リストを初期化
+
 	if (parse_exec_arg(argc, argv, &e_vars) == -1)
+	{
+		free_env_list(&e_vars.env_list); // <-- 失敗時も解放
 		return (EXIT_FAILURE);
+	}
+
 	if (e_vars.mode == INTERACTIVE)
 		exec_interactive(&e_vars);
 	// else
 	// 	exec_non_interactive(&e_vars);
+	free_env_list(&e_vars.env_list); // <-- 正常終了時に解放
 	return (EXIT_SUCCESS);
 }
