a5e3f47624e35f64496b369732504e6a5792191b
M2U7BF <yhvh4321@gmail.com>
2025-07-01 16:38:25 +0900

    update

--- src/util/ft_split_by_word_leave_separator.c
+++ src/util/ft_split_by_word_leave_separator.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 10:48:43 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/01 16:18:45 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/01 16:34:21 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,48 +27,43 @@ static void	free_all(char **result, int result_len)
 
 char	**ft_split_by_word_leave_separator(char *str, char *word)
 {
+	char	**res;
 	int		i;
 	int		j;
-	int		str_len;
+	int		len;
 	char	*word_position;
-	char	**result;
-	char	*tmp_str;
+	char	*tmp;
 
-	if (!str || !word || word[0] == '\0')
-		return (NULL);
-	str_len = ft_strlen(str);
-	result = malloc(sizeof(char *) * (str_len + 1));
-	if (!result)
-		return (NULL);
 	i = 0;
 	j = 0;
-	while (i < str_len)
+	len = ft_strlen(str);
+	if (!str || !word || !*word)
+		return (NULL);
+  res = malloc(sizeof(char *) * (len + 1));
+  if (!res)
+    return (NULL);
+	while (i < len)
 	{
-		word_position = ft_strnstr(str + i, word, str_len);
+		word_position = ft_strnstr(str + i, word, len - i);
 		if (word_position)
 		{
-			if (str + i != word_position)
+			if (word_position != str + i)
 			{
-				tmp_str = malloc(word_position - str + 1);
-				if (!tmp_str)
-					return (free_all(result, j - 1), NULL);
-				ft_strlcpy(tmp_str, str + i, word_position - str + i + 1);
-				result[j++] = tmp_str;
-				i += word_position - str + i;
+				tmp = malloc(word_position - (str + i) + 1);
+				if (!tmp)
+					return (free_all(res, j - 1), NULL);
+				ft_strlcpy(tmp, str + i, word_position - (str + i) + 1);
+				res[j++] = tmp;
 			}
-			result[j++] = ft_strdup(word);
-			i += ft_strlen(word);
+			res[j++] = ft_strdup(word);
+			i = word_position - str + ft_strlen(word);
 		}
 		else
 		{
-			tmp_str = malloc(str_len - i + 2);
-			if (!tmp_str)
-				return (free_all(result, j - 1), NULL);
-			ft_strlcpy(tmp_str, str + i, str_len - i + 2);
-			result[j++] = tmp_str;
-			i += str_len - i + 1;
+			res[j++] = ft_strdup(str + i);
+			break ;
 		}
 	}
-	result[j] = NULL;
-	return (result);
+	res[j] = NULL;
+	return (res);
 }
