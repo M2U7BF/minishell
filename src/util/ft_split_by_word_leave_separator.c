96ba7dea289fa88926b9273df2f7b75b66083bf3
M2U7BF <yhvh4321@gmail.com>
2025-07-01 14:05:02 +0900

    update

--- src/util/ft_split_by_word_leave_separator.c
+++ src/util/ft_split_by_word_leave_separator.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 10:48:43 by kkamei            #+#    #+#             */
-/*   Updated: 2025/06/30 09:45:18 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/01 13:13:06 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,10 @@ char	**ft_split_by_word_leave_separator(char *str, char *word)
 				count++;
 			}
 			i += ft_strlen(word);
-			count++;
 		}
 		else
-		{
 			i += str_len - i;
-			count++;
-		}
+    count++;
 	}
 	result = malloc(sizeof(char *) * (count + 1));
 	if (!result)
@@ -73,30 +70,21 @@ char	**ft_split_by_word_leave_separator(char *str, char *word)
 			{
 				result_str = malloc(word_position - str + 1);
 				if (!result_str)
-				{
-					free_all(result, j - 1);
-					return (NULL);
-				}
+					return (free_all(result, j - 1), NULL);
 				ft_strlcpy(result_str, str + i, word_position - (str + i) + 1);
-				result[j] = result_str;
-				j++;
+				result[j++] = result_str;
 				i += word_position - (str + i);
 			}
-			result[j] = ft_strdup(word);
+			result[j++] = ft_strdup(word);
 			i += ft_strlen(word);
-			j++;
 		}
 		else
 		{
 			result_str = malloc(str_len - i + 2);
 			if (!result_str)
-			{
-				free_all(result, j - 1);
-				return (NULL);
-			}
+				return (free_all(result, j - 1), NULL);
 			ft_strlcpy(result_str, str + i, str_len - i + 2);
-			result[j] = result_str;
-			j++;
+			result[j++] = result_str;
 			i += str_len - i + 1;
 		}
 	}
