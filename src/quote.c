c1929d10a812efee0bfd46bf88eee5530a4e9604
M2U7BF <yhvh4321@gmail.com>
2025-07-01 11:28:59 +0900

    update

--- src/quote.c
+++ src/quote.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 12:40:49 by kkamei            #+#    #+#             */
-/*   Updated: 2025/06/30 14:35:34 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/01 11:25:29 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,54 +27,57 @@ bool	is_quotation_error(char *input_line)
 	return (false);
 }
 
+static char	*remove_quotes(char *s)
+{
+	int		i;
+	int		j;
+	char	current_quote;
+	char	*result;
+
+	current_quote = '\0';
+	i = -1;
+	j = 0;
+	while (s[++i])
+	{
+		if (!current_quote && is_quote(s[i]))
+			current_quote = s[i];
+		else if (current_quote && current_quote == s[i])
+			current_quote = '\0';
+		else
+			j++;
+	}
+	result = malloc(sizeof(char) * (j + 1));
+	result[j] = '\0';
+	i = -1;
+	j = 0;
+	while (s[++i])
+	{
+		if (!current_quote && is_quote(s[i]))
+			current_quote = s[i];
+		else if (current_quote && current_quote == s[i])
+			current_quote = '\0';
+		else
+			result[j++] = s[i];
+	}
+	return (result);
+}
+
 // （bashの定義と同様に）クォーテーションを削除する。
 // 先頭から見て、ダブルクォートの数が偶数の場合、”ダブルクォートに囲まれていない”と判断する。
 void	quote_removal(t_token *token)
 {
-	int		i;
-	int		j;
 	t_token	*current;
-	char	current_quote;
-	char	*result;
+	char	*new;
 
 	current = token;
 	while (current && current->str)
 	{
 		if (current->type == WORD && (ft_strchr(current->str, '\'') != NULL
-					|| ft_strchr(current->str, '\"') != NULL))
+				|| ft_strchr(current->str, '\"') != NULL))
 		{
-			result = NULL;
-			current_quote = '\0';
-			i = -1;
-			j = 0;
-			while (current->str[++i])
-			{
-				if (!current_quote && is_quote(current->str[i]))
-					current_quote = current->str[i];
-				else if (current_quote && current_quote == current->str[i])
-					current_quote = '\0';
-				else
-					j++;
-			}
-			result = malloc(sizeof(char) * (j + 1));
-			result[j] = '\0';
-			current_quote = '\0';
-			i = -1;
-			j = 0;
-			while (current->str[++i])
-			{
-				if (!current_quote && is_quote(current->str[i]))
-					current_quote = current->str[i];
-				else if (current_quote && current_quote == current->str[i])
-					current_quote = '\0';
-				else
-				{
-					result[j] = current->str[i];
-					j++;
-				}
-			}
+			new = remove_quotes(current->str);
 			ft_free((void **)&current->str);
-			current->str = result;
+			current->str = new;
 		}
 		current = current->next;
 	}
