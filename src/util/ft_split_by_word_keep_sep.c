31847b21d187b77ebb94d9411f6d2c47a21418bc
M2U7BF <yhvh4321@gmail.com>
2025-07-02 13:40:43 +0900

    update

--- src/util/ft_split_by_word_keep_sep.c
+++ src/util/ft_split_by_word_keep_sep.c
@@ -6,36 +6,11 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 17:40:33 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/02 13:05:48 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/02 13:39:48 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-static void	free_all(char **result, int result_len)
-{
-	int	i;
-
-	i = 0;
-	while (result && i <= result_len && result[i])
-	{
-		ft_free((void **)&result[i]);
-		i++;
-	}
-	ft_free((void **)&result);
-}
-
-// wordに一致する部分の前の部分を取得
-static char	*get_not_word_part(char *word_position, char *str, int i)
-{
-	char	*tmp;
-
-	tmp = malloc(word_position - str + i + 1);
-	if (!tmp)
-		return (NULL);
-	ft_strlcpy(tmp, str + i, word_position - (str + i) + 1);
-	return (tmp);
-}
 
 char	**ft_split_by_word_keep_sep(char *str, char *word)
 {
@@ -43,34 +18,36 @@ char	**ft_split_by_word_keep_sep(char *str, char *word)
 	int		i;
 	int		j;
 	int		len;
-	char	*word_position;
+	char	*tmp;
+	int		k;
 
-	i = 0;
+	i = -1;
 	j = 0;
+	k = 0;
 	len = ft_strlen(str);
 	if (!str || !word || !*word)
 		return (NULL);
 	res = malloc(sizeof(char *) * (len + 1));
 	if (!res)
 		return (NULL);
-	while (i < len)
+	while (++i <= len)
 	{
-		word_position = ft_strnstr(str + i, word, len - i);
-		if (word_position)
+		if (ft_strncmp(str + i, word, ft_strlen(word)) == 0)
 		{
-			if (word_position != str + i)
+			if (i != 0 && i - k > 0)
 			{
-				res[j] = get_not_word_part(word_position, str, i);
-				if (!res[j++])
-					return (free_all(res, j - 1), NULL);
+				tmp = malloc(sizeof(char) * (i - k + 1));
+				ft_strlcpy(tmp, str + k, i - k + 1);
+				res[j++] = tmp;
 			}
 			res[j++] = ft_strdup(word);
-			i = word_position - str + ft_strlen(word);
+			k = i + ft_strlen(word);
 		}
-		else
+		else if (str[i] == '\0' && i - k > 0)
 		{
-			res[j++] = ft_strdup(str + i);
-			break ;
+			tmp = malloc(sizeof(char) * (i - k + 1));
+			ft_strlcpy(tmp, str + k, i - k + 1);
+			res[j++] = tmp;
 		}
 	}
 	res[j] = NULL;
