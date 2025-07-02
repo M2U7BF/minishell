1a789cda4ba30bc14f5035934bad206f614543f4
M2U7BF <yhvh4321@gmail.com>
2025-07-02 14:08:30 +0900

    norm修正

--- src/util/ft_split_by_word_keep_sep.c
+++ src/util/ft_split_by_word_keep_sep.c
@@ -6,42 +6,48 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 17:40:33 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/02 13:49:32 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/02 14:07:00 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+static void	split(int i, int *k, char *str, char *word, char ***res_tmp)
+{
+	char	*tmp;
+
+	if (i - *k > 0 && (is_str_equal(word, str + i, 0) || !str[i]))
+	{
+		tmp = malloc(sizeof(char) * (i - *k + 1));
+		ft_strlcpy(tmp, str + *k, i - *k + 1);
+		**res_tmp = tmp;
+		(*res_tmp)++;
+	}
+	if (is_str_equal(word, str + i, 0))
+	{
+		**res_tmp = ft_strdup(word);
+		(*res_tmp)++;
+		*k = i + ft_strlen(word);
+	}
+}
+
 char	**ft_split_by_word_keep_sep(char *str, char *word)
 {
-	char	**res;
+	char	**result;
+	char	**res_tmp;
 	int		i;
-	int		j;
-	char	*tmp;
 	int		k;
 
 	i = -1;
-	j = 0;
 	k = 0;
 	if (!str || !word || !*word)
 		return (NULL);
-	res = malloc(sizeof(char *) * (ft_strlen(str) + 1));
-	if (!res)
+	result = malloc(sizeof(char *) * (ft_strlen(str) + 1));
+	if (!result)
 		return (NULL);
+	res_tmp = result;
 	while (++i <= (int)ft_strlen(str))
-	{
-		if (i - k > 0 && (is_str_equal(word, str + i, 0) || !str[i]))
-		{
-			tmp = malloc(sizeof(char) * (i - k + 1));
-			ft_strlcpy(tmp, str + k, i - k + 1);
-			res[j++] = tmp;
-		}
-		if (is_str_equal(word, str + i, 0))
-		{
-			res[j++] = ft_strdup(word);
-			k = i + ft_strlen(word);
-		}
-	}
-	res[j] = NULL;
-	return (res);
+		split(i, &k, str, word, &res_tmp);
+	*res_tmp = NULL;
+	return (result);
 }
