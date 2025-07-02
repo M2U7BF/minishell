42e816e711a3d7a6e863395ba58711836c362899
M2U7BF <yhvh4321@gmail.com>
2025-07-02 13:49:56 +0900

    update

--- src/util/ft_split_by_word_keep_sep.c
+++ src/util/ft_split_by_word_keep_sep.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 17:40:33 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/02 13:46:40 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/02 13:49:32 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,18 @@ char	**ft_split_by_word_keep_sep(char *str, char *word)
 	char	**res;
 	int		i;
 	int		j;
-	int		len;
 	char	*tmp;
 	int		k;
 
 	i = -1;
 	j = 0;
 	k = 0;
-	len = ft_strlen(str);
 	if (!str || !word || !*word)
 		return (NULL);
-	res = malloc(sizeof(char *) * (len + 1));
+	res = malloc(sizeof(char *) * (ft_strlen(str) + 1));
 	if (!res)
 		return (NULL);
-	while (++i <= len)
+	while (++i <= (int)ft_strlen(str))
 	{
 		if (i - k > 0 && (is_str_equal(word, str + i, 0) || !str[i]))
 		{
