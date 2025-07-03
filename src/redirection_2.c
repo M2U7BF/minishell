05abc257ff1d126402d45156dddbf9351250d505
M2U7BF <yhvh4321@gmail.com>
2025-07-03 10:36:21 +0900

    update

--- src/redirection_2.c
+++ src/redirection_2.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 10:26:41 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/03 10:34:46 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/03 10:36:06 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,23 @@
 
 t_list	*pipe_redirect(t_proc_unit *proc, t_list *redirect_fds)
 {
-	int	target_fd;
 	int	*content;
 
 	if (!proc)
 		return (redirect_fds);
-	target_fd = 0;
 	if (proc->read_fd != STDIN_FILENO)
 	{
 		content = malloc(sizeof(int) * 2);
 		if (!content)
-		{
-			ft_lstclear(&redirect_fds, del_content);
-			return (NULL);
-		}
-    redirect(&proc->read_fd, STDIN_FILENO, &redirect_fds);
+			return (ft_lstclear(&redirect_fds, del_content), NULL);
+		redirect(&proc->read_fd, STDIN_FILENO, &redirect_fds);
 	}
 	if (proc->write_fd != STDOUT_FILENO)
 	{
 		content = malloc(sizeof(int) * 2);
 		if (!content)
-		{
-			ft_lstclear(&redirect_fds, del_content);
-			libc_error();
-		}
-    redirect(&proc->write_fd, STDOUT_FILENO, &redirect_fds);
+			return (ft_lstclear(&redirect_fds, del_content), NULL);
+		redirect(&proc->write_fd, STDOUT_FILENO, &redirect_fds);
 	}
 	return (redirect_fds);
 }
