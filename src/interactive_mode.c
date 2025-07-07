5a42e1e34770ea7ec9c9479c440fd82384222a4f
M2U7BF <yhvh4321@gmail.com>
2025-07-07 10:49:49 +0900

    終了ステータスの初期値を設定

--- src/interactive_mode.c
+++ src/interactive_mode.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 10:39:01 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/04 13:52:22 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/07 10:49:34 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	wait_child_processes(int *child_pids, int pro_count)
 	int	i;
 
 	i = -1;
+	status = g_vars.exit_status;
 	while (++i < pro_count)
 	{
 		if (child_pids[i] == -1)
