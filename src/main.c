fe14a9b074d1cdd3c41b7704acb332db2560ccb8
Atsuya Tashiro <atashiro@c1r4s7.42tokyo.jp>
2025-07-11 08:56:01 +0900

    rl_clear_history

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:39:07 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/04 11:54:40 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/10 18:01:22 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	main(int argc, char *argv[], char **envp)
 	{
 		init_i_vars(&e_vars.i_vars);
 		exec_interactive(&e_vars.i_vars);
-		clear_history();
+		rl_clear_history();
 	}
 	free_env_list(&g_vars.env_list);
 	return (EXIT_SUCCESS);
