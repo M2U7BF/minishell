bb8d410138676b306658e41b934deee1f93a628f
M2U7BF <yhvh4321@gmail.com>
2025-07-17 12:50:17 +0900

    norm修正

--- src/interactive_mode.c
+++ src/interactive_mode.c
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 10:39:01 by kkamei            #+#    #+#             */
-/*   Updated: 2025/07/17 12:25:38 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/07/17 12:48:29 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,21 @@ static void	process_ctrl_d(void)
 	exit(g_vars.exit_status);
 }
 
+static int	handle_input_value(t_i_mode_vars *i_vars)
+{
+	ft_free((void **)&i_vars->prompt);
+	if (g_vars.interrupted)
+		return (-1);
+	if (!i_vars->input)
+		process_ctrl_d();
+	else if (i_vars->input[0] == '\0')
+		return (-1);
+	add_history(i_vars->input);
+	ft_strtrim_front(&i_vars->input, DEFAULT_BLANK);
+	i_vars->token_list = tokenize(i_vars->input);
+	return (0);
+}
+
 int	exec_interactive(t_i_mode_vars *i_vars)
 {
 	t_proc_unit	*proc_list;
@@ -35,16 +50,8 @@ int	exec_interactive(t_i_mode_vars *i_vars)
 			ft_free((void **)&i_vars->input);
 		set_signal_handlers(false);
 		i_vars->input = readline(i_vars->prompt);
-		ft_free((void **)&i_vars->prompt);
-		if (g_vars.interrupted)
+		if (handle_input_value(i_vars) == -1)
 			continue ;
-		if (!i_vars->input)
-			process_ctrl_d();
-		else if (i_vars->input[0] == '\0')
-			continue ;
-		add_history(i_vars->input);
-		ft_strtrim_front(&i_vars->input, DEFAULT_BLANK);
-		i_vars->token_list = tokenize(i_vars->input);
 		if (!i_vars->token_list)
 			continue ;
 		if (parse(i_vars) == -1)
